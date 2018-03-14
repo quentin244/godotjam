1ead26b3d72f5eae065a93acada699fe7f3c1b0a
Quentin Joubert <quentin.joubert28@gmail.com>
2018-03-14 16:06:40 +0100

    modif

--- Lam/characters/DummyTest.gd
+++ Lam/characters/DummyTest.gd
@@ -7,8 +7,8 @@ signal direction_changed
 enum STATES { SPAWN, IDLE, BUMP }
 var state = null
 
-func _ready():
-	$Health.connect('health_changed', self, '_on_Health_health_changed')
+#func _ready():
+	#$Health.connect('health_changed', self, '_on_Health_health_changed')
 
 
 func _on_Weapon_attack_finished():
