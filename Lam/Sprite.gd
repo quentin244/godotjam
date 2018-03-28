2dd5822f30e1d85189cdffee21f80c8caef5ae6e
Quentin Joubert <quentin.joubert28@gmail.com>
2018-03-28 14:09:13 +0200

    Debug dialogue?

--- Lam/Sprite.gd
+++ Lam/Sprite.gd
@@ -3,16 +3,14 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
 		
 func dial():
-	var point = get_node("/root/Level/YSort/Player/Camera2D2/dialogue")
-	get_node("dialoguebox/dialoguebox")._print_dialogue("Hello World ")
-	get_node("dialoguebox/dialoguebox").show()
+	get_node("/root/Level/YSort/Player/Camera2D2/dialoguebox/dialoguebox")._print_dialogue("Hello World ")
+	get_node("/root/Level/YSort/Player/Camera2D2/dialoguebox/dialoguebox").show()
 	
 func closdial():
-	get_node("dialoguebox/dialoguebox")._reset_dialogue()
-	get_node("dialoguebox/dialoguebox").hide()
+	get_node("/root/Level/YSort/Player/Camera2D2/dialoguebox/dialoguebox")._reset_dialogue()
+	get_node("/root/Level/YSort/Player/Camera2D2/dialoguebox/dialoguebox").hide()
 
 func _on_Area2D_body_entered( body ):
 	if not body.is_in_group('player'):
@@ -21,4 +19,3 @@ func _on_Area2D_body_entered( body ):
 
 func _on_Area2D_body_exited( body ):
 	closdial()
-
