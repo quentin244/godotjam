8c4422365ba1f9e3e0f50cd6f5b254bdcb55766e
anozername <anozername@protonmail.com>
2018-03-28 21:39:09 +0200

    rng2

--- Connards/environment/map/assets/assetsRoot.gd
+++ Connards/environment/map/assets/assetsRoot.gd
@@ -1,33 +1,33 @@
 extends Node
 
 var taille = 8
-var x = 0
 var y = 0
+var x = 0
 var obj
 
 func _ready():
-	prints("taille :",taille)
-	while (y<taille*5-5):
+	while (y<taille*5-11):
 		if (y<(taille*5-5)/2):
 			spawn(preload("res://environment/map/assets/split/vege_10.tscn").instance(),10)
 			spawn(preload("res://environment/map/assets/split/vege_12.tscn").instance(),8)
 			spawn(preload("res://environment/map/assets/split/vege_13.tscn").instance(),7)
+			spawn(preload("res://environment/map/assets/split/sapin8.tscn").instance(),40)
 		else:
 			spawn(preload("res://environment/map/assets/split/vege_11.tscn").instance(),9)
 			spawn(preload("res://environment/map/assets/split/vege_14.tscn").instance(),6)
 			spawn(preload("res://environment/map/assets/split/vege_15.tscn").instance(),5)
 		spawn(preload("res://environment/map/assets/split/vege_16.tscn").instance(),4)
 		spawn(preload("res://environment/map/assets/split/vege_18.tscn").instance(),2)
+		y = y + 0.01*8
 	pass
 
 func spawn(ressource,proba):
 	randomize()
-	if (rand_range(0,10)<=proba):
+	if (rand_range(0,100)<=proba):
 		obj = Sprite.new()
 		randomize()
-		x = rand_range(512,taille*128)
-		y = rand_range(512,taille*128)
+		x = rand_range(0,taille*3+5)
 		obj = ressource
 		obj.z_index = obj.z_index+1 
 		add_child(obj)
-		obj.translate(Vector2(x,y))
+		obj.global_translate(Vector2(x*50+30,y*50+367))
