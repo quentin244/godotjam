9438014a2374e2dd2b6a2f17e05e5855e392a33c
anozername <anozername@protonmail.com>
2018-03-29 07:24:14 +0200

    j'ai tente, ca a pas marche. je laisse quand meme au cas ou

--- Connards/environment/map/assets/assetsRoot.gd
+++ Connards/environment/map/assets/assetsRoot.gd
@@ -4,22 +4,32 @@ var taille = 8
 var y = 0
 var x = 0
 var obj
+#var splitvege = [(preload("res://environment/map/assets/split/vege/vege_1.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_11.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_13.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_15.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_17.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_19.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_20.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_4.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_10.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_12.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_14.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_16.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_18.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_2.tscn").instance()),(preload("res://environment/map/assets/split/vege/vege_3.tscn").instance())]
+#var splitvegeused=[]
+
 
 func _ready():
+#	choice()
 	while (y<taille*5-10.5):
 		if (y<(taille*5-5)/2):
-			spawn(preload("res://environment/map/assets/split/vege_10.tscn").instance(),10)
-			spawn(preload("res://environment/map/assets/split/vege_12.tscn").instance(),8)
-			spawn(preload("res://environment/map/assets/split/vege_13.tscn").instance(),7)
+#			spawn(splitvegeused[randi()%splitvegeused.size()+1-1],8)
+			spawn(preload("res://environment/map/assets/split/png/sapin8.tscn").instance(),7)
+			spawn(preload("res://environment/map/assets/split/vege/vege_15.tscn").instance(),7)
+			spawn(preload("res://environment/map/assets/split/vege/vege_11.tscn").instance(),7)
 		else:
-			spawn(preload("res://environment/map/assets/split/vege_11.tscn").instance(),9)
-			spawn(preload("res://environment/map/assets/split/vege_14.tscn").instance(),6)
-			spawn(preload("res://environment/map/assets/split/vege_15.tscn").instance(),5)
-		spawn(preload("res://environment/map/assets/split/sapin8.tscn").instance(),100)
-		spawn(preload("res://environment/map/assets/split/vege_16.tscn").instance(),4)
-		spawn(preload("res://environment/map/assets/split/vege_18.tscn").instance(),2)
+			spawn(preload("res://environment/map/assets/split/vege/vege_19.tscn").instance(),9)
+			spawn(preload("res://environment/map/assets/split/vege/vege_18.tscn").instance(),6)
+			spawn(preload("res://environment/map/assets/split/png/souche_12.tscn").instance(),5)
+		spawn(preload("res://environment/map/assets/split/vege/vege_20.tscn").instance(),10)
+		spawn(preload("res://environment/map/assets/split/vege/vege_1.tscn").instance(),2)
 		y = y + 0.01*8
 	pass
+
+#func choice():
+#	for i in range(0,9):
+#		var rng = randi()%splitvege.size()+1-1
+#		splitvegeused.append(splitvege[rng])
+#		splitvege.erase(splitvege[rng])
 
 func spawn(ressource,proba):
 	randomize()
@@ -29,5 +39,7 @@ func spawn(ressource,proba):
 		x = rand_range(0,taille*3+5)
 		obj = ressource
 		obj.z_index = obj.z_index+1 
+		obj.set_name(str(randi()%999999+10))
 		add_child(obj)
 		obj.global_translate(Vector2(x*50+555,y*50+512))
+		prints("-------------------------")
